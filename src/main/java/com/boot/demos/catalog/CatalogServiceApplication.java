5b501047b1ee81a213b6a49931eec1b5c0ea9266
leandrodalbo <leandro.e.dal.bo@gmail.com>
2024-01-31 20:29:19 +0000

    starting with a plain service

---
+++ src/main/java/com/boot/demos/catalog/CatalogServiceApplication.java
@@ -0,0 +1,16 @@
+package com.boot.demos.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// @Configuration: mark a class as a source for beans definition
+// @ComponentScan: enables component scanning and register them to the application context
+// @EnableAutoConfiguration: triggered by several conditions (initialize embedded tomcat)
+public class CatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/demos/catalog/CatalogServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.boot.demos.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+//loads the full application context
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
