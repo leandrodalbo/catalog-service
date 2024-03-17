95b0c2fcb319d618591ae647291c7df45938273d
leandrodalbo <leandro.e.dal.bo@gmail.com>
2024-03-17 11:48:13 +0000

    integration tests with jdbc and test containers

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,10 @@ dependencies {
 
     runtimeOnly("org.postgresql:postgresql")
 
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.testcontainers:postgresql:1.19.3")
+    testImplementation("org.testcontainers:junit-jupiter:1.19.3")
 
 
 }
